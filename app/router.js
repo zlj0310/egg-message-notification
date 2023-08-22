1f3f24aa68e75e04765df6b825451600c2c0bb30
zlj0310 <zhulj0310@gmail.com>
2023-08-22 10:18:48 +0800

    优化

--- app/router.js
+++ app/router.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: lijun.zhu@huijie-inc.com
+ * @Date: 2023-08-21 10:02:41
+ * @LastEditors: lijun.zhu@huijie-inc.com
+ * @LastEditTime: 2023-08-22 10:18:31
+ * @Description: 
+ */
 'use strict';
 
 /**
@@ -6,5 +13,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.post('/sentry/webhook', controller.sentry.recvSentryWebhook);
+  router.get('/sentry/webhook', controller.sentry.recvSentryWebhook);
 };
